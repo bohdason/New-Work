0338b5c47957000b3bc93a6298dcb97977d84f0f
Bohdan Herasimchuk <bogdan.zaytsev.08@gmail.com>
2023-01-05 14:12:22 +0200

    add file

---
+++ js/carusel.js
@@ -0,0 +1,28 @@
+new Swiper('.swiper', {
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      
+
+      slidesPerView: 3,
+      loop: true,
+      spaceBetween: 30,
+
+
+
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+        }
+      },
+});

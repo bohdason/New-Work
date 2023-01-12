d3f8aef8b58f57bca7fa120a202cbad9398b70b9
Bohdan Herasimchuk <bogdan.zaytsev.08@gmail.com>
2023-01-12 16:40:17 +0200

    do modal more usefull

--- js/modal.js
+++ js/modal.js
@@ -9,12 +9,19 @@ const modal = document.getElementById('modal');
 open.addEventListener("click", function () {
 
    modal.classList.add("active");
+   document.body.classList.add("lock");
 });
 
 close.addEventListener("click", function () {
 
     modal.classList.remove("active");
+    document.body.classList.remove("lock");
  });
+
+ window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('active') : false
+);
+
  
 };
 

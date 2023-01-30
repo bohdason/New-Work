e405bb5db6fe3b8654f50005bec770c81c898908
Bohdan Herasimchuk <bogdan.zaytsev.08@gmail.com>
2023-01-30 11:59:21 +0200

    change link

--- js/curencies_calk.js
+++ js/curencies_calk.js
@@ -10,17 +10,18 @@
     async function caclulate() {
         const currency_one = currencyEl_one.value;
         const currency_two = currencyEl_two.value;
-        const response = await fetch(`https://api.exchangerate-api.com/v4latest/${currency_one}`)
+        const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`);
          const currencies = await response.json();
     
         const rate = currencies.rates[currency_two];
         rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
         amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    }
+    
     currencyEl_one.addEventListener('change', caclulate);
     amountEl_one.addEventListener('input', caclulate);
     currencyEl_two.addEventListener('change', caclulate);
     amountEl_two.addEventListener('input', caclulate);
+    }
 
     caclulate();
 })();
